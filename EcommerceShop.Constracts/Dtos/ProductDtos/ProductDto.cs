49f21abf1116f67cb7d31ae713da920c271aa28e
ImViet <dinhquocviet318@gmail.com>
2023-05-08 15:16:14 +0700

    Create productDto, pagingDto

---
+++ EcommerceShop.Constracts/Dtos/ProductDtos/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceShop.Contracts.Dtos.ProductDtos
+{
+    public class ProductDto
+    {
+    }
+}
