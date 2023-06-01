411c08984becd68aaed03e227b60172578398b39
ImViet <dinhquocviet318@gmail.com>
2023-06-01 23:53:38 +0700

    Refactor code

--- EcommerceShop.AdminApp/Services/RoleApiService.cs
+++ EcommerceShop.AdminApp/Services/RoleApiService.cs
@@ -1,4 +1,5 @@
 using System.Net.Http.Headers;
+using EcommerceShop.AdminApp.Extensions;
 using EcommerceShop.AdminApp.Interfaces;
 using EcommerceShop.Contracts;
 using EcommerceShop.Contracts.Dtos.RoleDtos;
@@ -6,26 +7,19 @@ using Newtonsoft.Json;
 
 namespace EcommerceShop.AdminApp.Services
 {
-    public class RoleApiService : IRoleService
+    public class RoleApiService : BaseHttpClientService, IRoleService
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IHttpClientFactory _httpClientFactory;
-        public RoleApiService(IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor)
+        public RoleApiService(IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor) 
+            : base(httpClientFactory, httpContextAccessor)
         {
-            _httpClientFactory = httpClientFactory;
-            _httpContextAccessor = httpContextAccessor;
         }
         public async Task<ApiResponse<List<RoleDto>>> GetAllRole()
         {
-            var token = _httpContextAccessor.HttpContext?.Session.GetString("Token");
-            var client = _httpClientFactory.CreateClient("myclient");
             var url = $"/api/role/getallrole";
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            var response = await client.GetAsync(url);
-            var data = await response.Content.ReadAsStringAsync();
-            if(response.IsSuccessStatusCode)
-                return JsonConvert.DeserializeObject<ApiSuccessResponse<List<RoleDto>>>(data);
-            return JsonConvert.DeserializeObject<ApiErrorResponse<List<RoleDto>>>(data);
+            var result = await GetAsync<ApiResponse<List<RoleDto>>>(url);
+            return result;
         }
     }
 }
