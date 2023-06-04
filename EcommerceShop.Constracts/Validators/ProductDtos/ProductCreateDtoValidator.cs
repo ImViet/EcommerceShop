e25941f6f45b05f664f0b0b64b577c0c95df4327
ImViet <dinhquocviet318@gmail.com>
2023-06-04 15:08:04 +0700

    BE: Get list category by language

--- EcommerceShop.Constracts/Validators/ProductDtos/ProductCreateDtoValidator.cs
+++ EcommerceShop.Constracts/Validators/ProductDtos/ProductCreateDtoValidator.cs
@@ -10,12 +10,12 @@ namespace EcommerceShop.Contracts.Validators.ProductDtos
         {
             RuleFor(x => x.Name).NotEmpty().WithMessage("Nhập tên sản phẩm");
             RuleFor(x => x.Price).NotEmpty().WithMessage("Nhập giá bán");
-            // RuleFor(x => x.OriginalPrice).NotEmpty().WithMessage("Nhập tên sản phẩm");
-            // RuleFor(x => x.Details).NotEmpty().WithMessage("Nhập tên sản phẩm");
-            // RuleFor(x => x.Description).NotEmpty().WithMessage("Nhập tên sản phẩm");
-            // RuleFor(x => x.SeoAlias).NotEmpty().WithMessage("Nhập tên sản phẩm");
-            // RuleFor(x => x.SeoTitle).NotEmpty().WithMessage("Nhập tên sản phẩm");
-            // RuleFor(x => x.SeoDescription).NotEmpty().WithMessage("Nhập tên sản phẩm");
+            RuleFor(x => x.OriginalPrice).NotEmpty().WithMessage("Nhập giá gốc");
+            RuleFor(x => x.Details).NotEmpty().WithMessage("Nhập chi tiết");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Nhập mô tả");
+            RuleFor(x => x.SeoAlias).NotEmpty().WithMessage("Nhập Seo alias");
+            RuleFor(x => x.SeoTitle).NotEmpty().WithMessage("Nhập Seo title");
+            RuleFor(x => x.SeoDescription).NotEmpty().WithMessage("Nhập mô tả Seo");
         }
     }
 }
